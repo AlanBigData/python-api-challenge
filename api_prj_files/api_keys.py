0ce083ad745a43aeb9047b6cb54e65ca2baf7a53
AlanBigData <alawrence026@gmail.com>
2023-12-18 00:59:56 -0500

    updated code

--- api_prj_files/api_keys.py
+++ api_prj_files/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "be71eada90b77396fa3728614e384773"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "ee2c6ace713d4289bd5d1885b1193c4c"
